6e10a5f5a02c78c7fd9a5e35a39963d665035d6a
Alexander <vsany@meta.ua>
2024-04-18 14:04:58 +0300

    [+] Country model [+] Resource model

--- Assets/Models/CountryModel.cs
+++ Assets/Models/CountryModel.cs
@@ -1,13 +1,16 @@
 using System;
+using System.Collections.Generic;
+using JetBrains.Annotations;
 
 namespace Models
 {
     [Serializable]
     public class CountryModel
     {
+        [CanBeNull]
         public string Country;
-        public bool isCapital;
-        public bool isOccupied;
-        
+        public bool? isCapital;
+        public bool? isOccupied;
+        public ResourceModel Resources { get; set; }
     }
 }
--- Assets/Models/ResourceModel.cs
+++ Assets/Models/ResourceModel.cs
@@ -2,12 +2,24 @@ namespace Models
 {
     public class ResourceModel
     {
-        public int Bronze;
+        public int Bronze {get; set; }
 
         public int Silver;
 
         public int Gold;
 
         public int DonatCrystal;
+
+
+        public static ResourceModel CreateEmpty()
+        {
+            return new ResourceModel()
+            {
+                Bronze = 0,
+                Silver = 0,
+                Gold = 0,
+                DonatCrystal = 0
+            };
+        }
     }
 }
